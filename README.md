9c0e8a87c62962713faed925861efe9cea807cb6
deekshasharma <deeksha.sharma25@gmail.com>
2020-03-30 10:05:11 -0700

    Added a new component to  unmount Quote

---
+++ README.md
@@ -0,0 +1,5 @@
+## Introduction
+The goal of this project is to have a code example implemented using `useEffect` hook in React.
+
+
+### What this code does
--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-/**
- * Goal:
- * 1. User is by default subscribed to daily quotes.
- * 2. On each refresh there should be a new quote on the screen.
- * 3. When user clicks on Unsubscribe button, it should display "You are not subscribed to the quotes"
- * and there should be no quote on the screen.Additionally user should now see "SUBSCRIBE" button.
- * 4. When user clicks the "SUBSCRIBE" button , the quotes should start to displayaand every refresh should render a new quote.
- */
+import { Poem } from "./Poem";
+import Quote from "./Quote";
 
 function App() {
-  const [quote, setQuote] = useState("");
-  const [quotes, setQuotes] = useState([]);
+  const [showQuotes, setQuotes] = useState(true);
+  const [showPoems, setPoems] = useState(false);
 
-  useEffect(() => {
-    fetch("https://api.quotable.io/quotes?limit=10")
-      .then(response => response.json())
-      .then(jsonResponse => {
-        setQuotes(jsonResponse.results);
-      });
-  }, []);
+  const onClickPoems = () => {
+    setPoems(true);
+    setQuotes(false);
+  };
 
-  useEffect(() => {
-    getQuote();
-  }, [quotes]);
-
-  const getQuote = () => {
-    let id = Math.floor(Math.random() * 9) + 0;
-    setQuote(quotes.length > 0 ? quotes[id]["content"] : "");
+  const onClickQuotes = () => {
+    setQuotes(true);
+    setPoems(false);
   };
 
   return (
     <div className="App">
-      <h1>Quote of the Day!</h1>
-      <h3>{quote}</h3>
-      <button onClick={() => console.log("un")}>UNSUBSCRIBE</button>
-      <button onClick={getQuote}>NEXT QUOTE</button>
+      <button className="quotes-button" onClick={onClickQuotes}>QUOTES</button>
+      <button className="poems-button" onClick={onClickPoems}>POEMS</button>
+      {showPoems && <Poem />}
+      {showQuotes && <Quote />}
     </div>
   );
 }
 
 export default App;
-
-const Unsub = () => {
-  return (
-    <h3>You have unsubscribed! To view quotes click "SUBSCRIBE" button</h3>
-  );
-};

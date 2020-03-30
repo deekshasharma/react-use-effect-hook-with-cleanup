5cac83448df52dbccae12679427749bddfb38edd
deekshasharma <deeksha.sharma25@gmail.com>
2020-03-29 17:59:38 -0700

    Added an example code for using effects hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+/**
+ * Goal:
+ * 1. User is by default subscribed to daily quotes.
+ * 2. On each refresh there should be a new quote on the screen.
+ * 3. When user clicks on Unsubscribe button, it should display "You are not subscribed to the quotes"
+ * and there should be no quote on the screen.Additionally user should now see "SUBSCRIBE" button.
+ * 4. When user clicks the "SUBSCRIBE" button , the quotes should start to displayaand every refresh should render a new quote.
+ */
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/quotes?limit=10")
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setQuotes(jsonResponse.results);
+      });
+  }, []);
+
+  useEffect(() => {
+    getQuote();
+  }, [quotes]);
+
+  const getQuote = () => {
+    let id = Math.floor(Math.random() * 9) + 0;
+    setQuote(quotes.length > 0 ? quotes[id]["content"] : "");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quote of the Day!</h1>
+      <h3>{quote}</h3>
+      <button onClick={() => console.log("un")}>UNSUBSCRIBE</button>
+      <button onClick={getQuote}>NEXT QUOTE</button>
     </div>
   );
 }
 
 export default App;
+
+const Unsub = () => {
+  return (
+    <h3>You have unsubscribed! To view quotes click "SUBSCRIBE" button</h3>
+  );
+};
